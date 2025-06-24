681d259178bbdab5e18d4138c79673b272453d29
gxuu <georgexu420@163.com>
2025-06-24 22:33:38 +0800

    Update event loop type requiment
    - Alternative is to use CRTP. With CRTP we at least get the benefit
    of interface constraining. I choose this method mainly because I don't
    want the backend to inherit from the base class, as it doesn't make
    sense.
    
    Signed-off-by: gxuu <georgexu420@163.com>
    
    Author:    gxuu <georgexu420@163.com>

--- scaler/io/ymq/event_loop.h
+++ scaler/io/ymq/event_loop.h
@@ -1,38 +1,50 @@
 #pragma once
 
 // C++
+#include <concepts>
 #include <cstdint>  // uint64_t
 #include <functional>
 
 // First-party
+#include "scaler/io/ymq/configuration.h"
 #include "scaler/io/ymq/epoll_context.h"
 
 struct Timestamp;
 class EventManager;
 
-template <class EventLoopBackend = EpollContext>
-struct EventLoop {
-    using Function   = std::function<void()>;  // TBD
-    using Identifier = int;                    // TBD
-    void loop() { eventLoopBackend.loop(); }
+template <class T>
+concept EventLoopBackend = requires(T t, std::function<void()> f) {
+    { t.executeNow(f) } -> std::same_as<void>;
+    { t.executeLater(f) } -> std::same_as<void>;
+    { t.executeAt(Timestamp {}, f) } -> std::integral;
+    { t.cancelExecution(0) } -> std::same_as<void>;
 
-    void executeNow(Function func) { eventLoopBackend.executeNow(std::move(func)); }
-    void executeLater(Function func) { eventLoopBackend.executeLater(std::move(func)); }
+    t.addFdToLoop(int {}, uint64_t {}, (EventManager*)nullptr);
+    { t.removeFdFromLoop(int {}) } -> std::same_as<void>;
+};
 
-    Identifier executeAt(Timestamp timestamp, Function func) {
-        return eventLoopBackend.executeAt(timestamp, std::move(func));
-    }
-    void cancelExecution(Identifier identifier) { eventLoopBackend.cancelExecution(identifier); }
+template <EventLoopBackend Backend = EpollContext>
+class EventLoop {
+    Backend backend;
+
+public:
+    using Function   = std::function<void()>;
+    using Identifier = Configuration::ExecutionCancellationIdentifier;
+    void loop() { backend.loop(); }
+
+    void executeNow(Function func) { backend.executeNow(std::move(func)); }
+    void executeLater(Function func) { backend.executeLater(std::move(func)); }
+
+    Identifier executeAt(Timestamp timestamp, Function func) { return backend.executeAt(timestamp, std::move(func)); }
+    void cancelExecution(Identifier identifier) { backend.cancelExecution(identifier); }
 
     // NOTE: These two functions are not used. - gxu
     void registerCallbackBeforeLoop(EventManager*);
-    void registerEventManager(EventManager& em) { eventLoopBackend.registerEventManager(em); }
+    void registerEventManager(EventManager& em) { backend.registerEventManager(em); }
 
     auto addFdToLoop(int fd, uint64_t events, EventManager* manager) {
-        return eventLoopBackend.addFdToLoop(fd, events, manager);
+        return backend.addFdToLoop(fd, events, manager);
     }
 
-    void removeFdFromLoop(int fd) { eventLoopBackend.removeFdFromLoop(fd); }
-
-    EventLoopBackend eventLoopBackend;
+    void removeFdFromLoop(int fd) { backend.removeFdFromLoop(fd); }
 };
