af10dcb100e9e7e20b88dd29ba223f6d3d15c978
gxu <georgexu420@163.com>
2025-07-03 05:14:44 +0800

    Provide basic examples on how to use YMQ in C++
    Signed-off-by: gxu <georgexu420@163.com>

---
+++ scaler/io/ymq/examples/echo_client.cpp
@@ -0,0 +1,78 @@
+
+// C++
+#include <stdio.h>
+#include <unistd.h>
+
+#include <future>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include "scaler/io/ymq/io_context.h"
+#include "scaler/io/ymq/io_socket.h"
+#include "scaler/io/ymq/typedefs.h"
+
+int main() {
+    IOContext context;
+
+    auto createSocketPromise               = std::make_shared<std::promise<void>>();
+    auto createSocketFuture                = createSocketPromise->get_future();
+    std::shared_ptr<IOSocket> clientSocket = context.createIOSocket(
+        "ClientSocket", IOSocketType::Dealer, [createSocketPromise] { createSocketPromise->set_value(); });
+
+    createSocketFuture.wait();
+    printf("Successfully created socket.\n");
+
+    auto connect_promise = std::make_shared<std::promise<void>>();
+    auto connect_future  = connect_promise->get_future();
+
+    clientSocket->connectTo("tcp://127.0.0.1:8080", [connect_promise](int result) { connect_promise->set_value(); });
+
+    printf("Waiting for connection...\n");
+    connect_future.wait();
+    printf("Connected to server.\n");
+
+    int cnt = 0;
+    while (cnt++ < 10) {
+        std::string line;
+        std::cout << "Enter a message to send: ";
+        if (!std::getline(std::cin, line)) {
+            std::cout << "EOF or input error. Exiting...\n";
+            break;
+        }
+        std::cout << "YOU ENTERED THIS MESSAGE: " << line << std::endl;
+
+        Message message;
+        std::string destAddress = "ServerSocket";
+
+        message.address = Bytes {const_cast<char*>(destAddress.c_str()), destAddress.size()};
+
+        message.payload = Bytes {const_cast<char*>(line.c_str()), line.size()};
+
+        auto send_promise = std::make_shared<std::promise<void>>();
+        auto send_future  = send_promise->get_future();
+
+        clientSocket->sendMessage(std::move(message), [send_promise](int) { send_promise->set_value(); });
+
+        send_future.wait();
+        printf("Message sent, waiting for response...\n");
+
+        auto recv_promise = std::make_shared<std::promise<Message>>();
+        auto recv_future  = recv_promise->get_future();
+
+        clientSocket->recvMessage([recv_promise](Message msg) { recv_promise->set_value(std::move(msg)); });
+
+        Message reply = recv_future.get();
+        std::string reply_str(reply.payload.data(), reply.payload.data() + reply.payload.len());
+        printf("Received echo: '%s'\n", reply_str.c_str());
+    }
+
+    // TODO: remove IOSocket also needs a future
+    context.removeIOSocket(clientSocket);
+
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(100ms);
+
+    return 0;
+}
--- scaler/io/ymq/examples/echo_server.cpp
+++ scaler/io/ymq/examples/echo_server.cpp
@@ -1,25 +1,61 @@
-// C
-#include <stdio.h>
 
-// First-party
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <future>
+#include <memory>
+
 #include "scaler/io/ymq/io_context.h"
 #include "scaler/io/ymq/io_socket.h"
-#include "scaler/io/ymq/typedefs.h"
-
-// Goal:
-// Make sure we can write an echo server with ymq in C++, pretend there is a language barrier, to mimic
-// the behavior as if we are running with Python
-// We should of course provide an echo client.
 
 int main() {
-    printf("Hello, world!\n");
+    auto createSocketPromise = std::make_shared<std::promise<void>>();
+    auto createSocketFuture  = createSocketPromise->get_future();
 
     IOContext context;
-    std::shared_ptr<IOSocket> socket = context.createIOSocket("ServerSocket", IOSocketType::Dealer);
-    // char buf[8];
-    // while (true) {
-    //     socket.read("any_identity", buf, []() { printf("read completed\n"); });
-    //     socket.write("reading_from_identity", buf, []() { printf("write completed\n"); });
-    // }
-    // printf("done");
+    std::shared_ptr<IOSocket> socket = context.createIOSocket(
+        "ServerSocket", IOSocketType::Dealer, [createSocketPromise] { createSocketPromise->set_value(); });
+
+    createSocketFuture.wait();
+    printf("Successfully created socket.\n");
+    // using namespace std::chrono_literals;
+    // std::this_thread::sleep_for(100ms);
+
+    auto bind_promise = std::make_shared<std::promise<void>>();
+    auto bind_future  = bind_promise->get_future();
+
+    socket->bindTo("tcp://127.0.0.1:8080", [bind_promise](int result) {
+        // Optionally handle result
+        bind_promise->set_value();
+    });
+
+    printf("Waiting for bind to complete...\n");
+    bind_future.wait();
+    printf("Successfully bound socket\n");
+
+    while (true) {
+        printf("Try to recv a message\n");
+
+        auto recv_promise = std::make_shared<std::promise<Message>>();
+        auto recv_future  = recv_promise->get_future();
+
+        socket->recvMessage([socket, recv_promise](Message msg) { recv_promise->set_value(std::move(msg)); });
+
+        Message received_msg = recv_future.get();
+        printf(
+            "Receiving message from '%s', message content is: '%s'\n",
+            received_msg.address.as_string().c_str(),
+            std::string(received_msg.payload.data(), received_msg.payload.data() + received_msg.payload.len()).c_str());
+
+        auto send_promise = std::make_shared<std::promise<void>>();
+        auto send_future  = send_promise->get_future();
+
+        socket->sendMessage(std::move(received_msg), [send_promise](int) { send_promise->set_value(); });
+
+        send_future.wait();
+        printf("Message echoed back. Looping...\n");
+    }
+
+    return 0;
 }
