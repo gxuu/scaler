ed63f384b6dbccf8a4fbba89bd4a8df5e45db91d
magniloquency <197707854+magniloquency@users.noreply.github.com>
2025-07-13 23:23:51 -0400

    update tester
    Signed-off-by: magniloquency <197707854+magniloquency@users.noreply.github.com>

--- scaler/io/ymq/ymq_test.py
+++ scaler/io/ymq/ymq_test.py
@@ -8,3 +8,8 @@ print(context, ";", socket)
 assert context.num_threads == 2
 assert socket.identity == "my-socket"
 assert socket.socket_type == ymq.IOSocketType.Dealer
+
+exc = ymq.YMQException(ymq.ErrorCode.InvalidAddressFormat, "oh no!")
+assert exc.code == ymq.ErrorCode.InvalidAddressFormat
+assert exc.message == "oh no!"
+assert exc.code.explanation()
