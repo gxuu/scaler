187e2c8a3c5f49eea6905cb05c7660cbe4cbcc5a
magniloquency <197707854+magniloquency@users.noreply.github.com>
2025-07-15 21:49:24 -0400

    rename hook
    Signed-off-by: magniloquency <197707854+magniloquency@users.noreply.github.com>

--- scaler/io/ymq/pymod_ymq/ymq.cpp
+++ scaler/io/ymq/pymod_ymq/ymq.cpp
@@ -3,7 +3,7 @@
 #include "scaler/io/ymq/pymod_ymq/ymq.h"
 #include "scaler/io/ymq/error.h"
 
-constexpr inline void myUnrecoverableError(scaler::ymq::Error e) {
+constexpr inline void ymqUnrecoverableError(scaler::ymq::Error e) {
     PyGILState_STATE gstate = PyGILState_Ensure();
     PyErr_SetString(PyExc_SystemExit, e.what());
     PyErr_WriteUnraisable(nullptr);
