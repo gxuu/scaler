0e0bad627ab99e1442780aa5e8abfaea1d698a72
magniloquency <197707854+magniloquency@users.noreply.github.com>
2025-07-21 20:24:57 -0400

    format test file
    Signed-off-by: magniloquency <197707854+magniloquency@users.noreply.github.com>

--- scaler/io/ymq/ymq_test.py
+++ scaler/io/ymq/ymq_test.py
@@ -1,9 +1,10 @@
 import asyncio, ymq
 
+
 async def main():
     ctx = ymq.IOContext()
     socket = await ctx.createIOSocket("ident", ymq.IOSocketType.Binder)
-    print(ctx, ';', socket)
+    print(ctx, ";", socket)
 
     assert socket.identity == "ident"
     assert socket.socket_type == ymq.IOSocketType.Binder
@@ -12,5 +13,6 @@ async def main():
     assert exc.code == ymq.ErrorCode.InvalidAddressFormat
     assert exc.message == "oh no!"
     assert exc.code.explanation()
+
 
 asyncio.run(main())
