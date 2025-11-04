63cfa7ff6039004b301f253a59345ec2e7877861
gxu <georgexu420@163.com>
2025-11-05 06:15:45 +0800

    Let TcpClient uses RawClientTCPFD
    Signed-off-by: gxu <georgexu420@163.com>
    
    CLIENT basic impl
    
    Signed-off-by: gxu <georgexu420@163.com>
    
    CLIENT pass windows
    
    Signed-off-by: gxu <georgexu420@163.com>
    
    CLIENT pass windows
    
    Signed-off-by: gxu <georgexu420@163.com>
    
    CLIENT don't remove for Windows
    
    Signed-off-by: gxu <georgexu420@163.com>

---
+++ src/cpp/scaler/ymq/internal/raw_client_tcp_fd.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <cstdint>  // uint64_t
+
+#include "scaler/ymq/internal/defs.h"
+
+namespace scaler {
+namespace ymq {
+
+class RawClientTCPFD {
+public:
+    RawClientTCPFD(sockaddr remoteAddr);
+    ~RawClientTCPFD();
+
+    RawClientTCPFD(RawClientTCPFD&&)                  = delete;
+    RawClientTCPFD& operator=(RawClientTCPFD&& other) = delete;
+    RawClientTCPFD(const RawClientTCPFD&)             = delete;
+    RawClientTCPFD& operator=(const RawClientTCPFD&)  = delete;
+
+    void create();
+    void destroy();
+    bool prepConnect(void* notifyHandle);
+    bool needRetry();
+
+    void zeroNativeHandle() noexcept;
+
+    auto nativeHandle() const noexcept
+    {
+#ifdef _WIN32
+        return (SOCKET)_clientFD;
+#endif  //_WIN32
+#ifdef __linux__
+        return (int)_clientFD;
+#endif
+    }
+
+private:
+    uint64_t _clientFD;
+    sockaddr _remoteAddr;
+#ifdef _WIN32
+    LPFN_CONNECTEX _connectExFunc;
+#endif  // _WIN32
+};
+
+}  // namespace ymq
+}  // namespace scaler
