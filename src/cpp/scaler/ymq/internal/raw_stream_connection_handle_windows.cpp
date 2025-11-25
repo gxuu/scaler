bde06119aba6b9fb3174958ead5d26bf1dff426c
gxu <georgexu420@163.com>
2025-11-26 06:07:07 +0800

    Remove defs.h
    Signed-off-by: gxu <georgexu420@163.com>

--- src/cpp/scaler/ymq/internal/raw_stream_connection_handle_windows.cpp
+++ src/cpp/scaler/ymq/internal/raw_stream_connection_handle_windows.cpp
@@ -3,9 +3,22 @@
 #include <cassert>  // assert
 
 #include "scaler/error/error.h"
-#include "scaler/ymq/internal/defs.h"
 #include "scaler/ymq/internal/network_utils.h"
 #include "scaler/ymq/internal/raw_stream_connection_handle.h"
+
+// clang-format off
+#define NOMINMAX
+#include <windows.h>
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h> // inet_pton
+// clang-format on
+
+#undef SendMessageCallback
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#define EPOLLIN             (0)
+#define EPOLLOUT            (0)
+#define EPOLLET             (0)
 
 namespace scaler {
 namespace ymq {
