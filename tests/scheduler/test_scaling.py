aa5a2eeaeaa91c73e45dbd4ad52a89e46b13d524
magniloquency <197707854+magniloquency@users.noreply.github.com>
2026-01-20 20:22:00 -0500

    rename policies.allocate_policy to policies.allocation

--- tests/scheduler/test_scaling.py
+++ tests/scheduler/test_scaling.py
@@ -37,7 +37,7 @@ from scaler.protocol.python.message import InformationSnapshot, Task, WorkerHear
 from scaler.protocol.python.status import Resource
 from scaler.scheduler.controllers.policies.scaling_policies.capability_scaling import CapabilityScalingController
 from scaler.utility.identifiers import ClientID, ObjectID, TaskID, WorkerID
-from scaler.scheduler.controllers.policies.allocate_policy.allocate_policy import AllocatePolicy
+from scaler.scheduler.controllers.policies.allocation.allocate_policy import AllocatePolicy
 from scaler.scheduler.controllers.policies.scaling_policies.types import ScalingControllerStrategy
 from scaler.utility.logging.utility import setup_logger
 from scaler.utility.network_util import get_available_tcp_port
