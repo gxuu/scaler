16b0cb8f1a3d2ccb9bfb34539bdf82380e9162e7
gxu <georgexu420@163.com>
2026-01-30 04:48:20 +0800

    Make ScalingControllers stateless
    - remove the RESTful API that was in the scaling controller. Original
      implementation is directly making call to remote end to start or
      shutdown worker group. Now, the implementation is stateless in the
      scalingControllers. A new type called WoerkAdapterController is being
      used to handle the communication between local (scheduler) and  remote
      (worker adapters).
    - Most scaling controller are now stateless with the exception being
      `FixedElasticScalingController`. This is expected as the underlying
      requirement for this and other controllers are quite different.
    - Since this PR removed the RESTful API, all the corresponding code
      related to aiohttp was removed.
    - The mock test that mock web request was also removed.
    - We have two pair of new capnp message types being
      WorkerAdapterHeartBeat/WorkerAdapterHeartBeatEcho, and
      WorkerAdapterCommand/WorkerAdapterCommandResponse. Information flows:
      WorkerAdapter ---WorkerAdapterHeartbeat-> Scheduler
      Scheduler --WorkerAdapterHeartbeatEcho -> WorkerAdapter
      Scheduler --[optionally] Command -> WorkerAdapter
      WorkerAdapter ---Resopnse-> Scheduler
    
    Signed-off-by: gxu <georgexu420@163.com>

--- src/scaler/scheduler/controllers/policies/simple_policy/scaling/mixins.py
+++ src/scaler/scheduler/controllers/policies/simple_policy/scaling/mixins.py
@@ -1,10 +1,54 @@
 import abc
+from typing import List, Tuple
 
-from scaler.protocol.python.message import InformationSnapshot
-from scaler.utility.mixins import Reporter
+from scaler.protocol.python.message import (
+    InformationSnapshot,
+    WorkerAdapterCommand,
+    WorkerAdapterCommandResponse,
+    WorkerAdapterHeartbeat,
+)
+from scaler.protocol.python.status import ScalingManagerStatus
+from scaler.scheduler.controllers.policies.simple_policy.scaling.types import WorkerGroupCapabilities, WorkerGroupState
 
 
-class ScalingController(Reporter):
+class ScalingController:
+    """
+    Stateless scaling controller interface.
+
+    All state (worker groups, capabilities) is owned by WorkerAdapterController and passed in as parameters.
+    Controllers return commands and updated state rather than mutating internal state.
+    """
+
     @abc.abstractmethod
-    async def on_snapshot(self, snapshot: InformationSnapshot):
+    def get_scaling_commands(
+        self,
+        snapshot: InformationSnapshot,
+        adapter_heartbeat: WorkerAdapterHeartbeat,
+        worker_groups: WorkerGroupState,
+        worker_group_capabilities: WorkerGroupCapabilities,
+    ) -> List[WorkerAdapterCommand]:
+        """
+        Pure function: state in, commands out.
+
+        Returns a list of WorkerAdapterCommands. Commands are either all start or all shutdown, never mixed.
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def on_scaling_command_response(
+        self,
+        response: WorkerAdapterCommandResponse,
+        worker_groups: WorkerGroupState,
+        worker_group_capabilities: WorkerGroupCapabilities,
+    ) -> Tuple[WorkerGroupState, WorkerGroupCapabilities]:
+        """
+        Pure function: takes current state + response, returns updated state.
+
+        Returns (updated_worker_groups, updated_worker_group_capabilities).
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def get_status(self, worker_groups: WorkerGroupState) -> ScalingManagerStatus:
+        """Pure function: state in, status out."""
         raise NotImplementedError()
